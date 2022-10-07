b56d60b13a8f35efa15559d1b0f3dd647942c047
Tien Duc Le <6162574+tienducle@users.noreply.github.com>
2022-10-07 09:14:20 +0200

    Updated ProtonGE download scripts

---
+++ src/legacyprotonge.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# GE-Proton7-15
+./protonge.sh 65048928
+
+# GE-Proton7-21
+./protonge.sh 70569061
+
+# GE-Proton7-27
+./protonge.sh 72828013
--- src/protonge.sh
+++ src/protonge.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
 # The script will keep this max number of Proton GE versions. Older versions (determined by folder's last modified timestamp) will be deleted.
-MAX_NUMBER_OF_VERSIONS_INSTALLED=5
+MAX_NUMBER_OF_VERSIONS_INSTALLED=100
 
 # Keep downloaded .tar.gz asset
 KEEP_DOWNLOADED_ASSET=false
 
-# URL of Proton GE releases
-RELEASES_URL="https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases"
+# URL of Proton GE latest release
+if [[ -z "$1" ]]; then
+  LATEST_RELEASE_URL="https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest"
+else
+  LATEST_RELEASE_URL="https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/$1"
+fi
 
 # Target folder to extract Proton GE into
 TARGET_FOLDER=$(eval echo "~/.steam/root/compatibilitytools.d")
 
-# Get the list of releases (newest first)
-echo "Getting releases from $RELEASES_URL.."
-RELEASES=$(curl $RELEASES_URL)
+# Get release details
+echo "Getting release details from $LATEST_RELEASE_URL.."
+LATEST_RELEASE_JSON=$(curl $LATEST_RELEASE_URL)
 
-# Get assets of first (most recent) release
-ASSETS=$(jq '.[0]["assets"] | length' <<< "$RELEASES")
+# Get assets of release
+ASSETS_COUNT=$(jq '.["assets"] | length' <<< "$LATEST_RELEASE_JSON")
 
 # Find the tar.gz release asset
-for (( i=0; i<$ASSETS; i++ ))
+for (( i=0; i<$ASSETS_COUNT; i++ ))
 do
-	ASSET_NAME=$(jq --arg index "$i" '.[0]["assets"][$index|tonumber] | select(.name | endswith(".tar.gz")) | .name' <<< "$RELEASES" | tr -d \")
-	ASSET_ID=$(jq --arg index "$i" '.[0]["assets"][$index|tonumber] | select(.name | endswith(".tar.gz")) | .id' <<< "$RELEASES")
+	ASSET_NAME=$(jq --arg index "$i" '.["assets"][$index|tonumber] | select(.name | endswith(".tar.gz")) | .name' <<< "$LATEST_RELEASE_JSON" | tr -d \")
+	ASSET_ID=$(jq --arg index "$i" '.["assets"][$index|tonumber] | select(.name | endswith(".tar.gz")) | .id' <<< "$LATEST_RELEASE_JSON")
 done
 
 # If the asset was found, check if the current version is up-to-date
@@ -41,36 +45,34 @@ then
 	echo $ALREADY_DOWNLOADED
 	echo "Downloading $ASSET_NAME.."
 	curl -L -H "Accept: application/octet-stream" "https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/assets/$ASSET_ID" -o "$ASSET_NAME"
+	# Extract
+	echo "Extracting to ~/.steam/root/compatibilitytools.d/"
+	mkdir -p ~/.steam/root/compatibilitytools.d
+	tar -xf "$ASSET_NAME" -C ~/.steam/root/compatibilitytools.d/
+
+	# Delete downloaded asset if enabled
+	if ! $KEEP_DOWNLOADED_ASSET
+	then
+		echo "Deleting downloaded asset $ASSET_NAME.."
+		rm -rf $ASSET_NAME
+	fi
 else
-	echo "Latest version is already installed."
-	exit 1;
-fi
-
-# Extract
-echo "Extracting to ~/.steam/root/compatibilitytools.d/"
-mkdir -p ~/.steam/root/compatibilitytools.d
-tar -xf "$ASSET_NAME" -C ~/.steam/root/compatibilitytools.d/
-
-# Delete downloaded asset if enabled
-if ! $KEEP_DOWNLOADED_ASSET
-then
-	echo "Deleting downloaded asset $ASSET_NAME.."
-	rm -rf $ASSET_NAME
+	echo "$ASSET_NAME is already installed."
+#	exit 1;
 fi
 
 # Cleanup of old versions
 echo "Cleanup and keeping $MAX_NUMBER_OF_VERSIONS_INSTALLED latest Proton GE versions.."
-INSTALLED=$(ls -t $TARGET_FOLDER | grep GE-Proton)
-declare -a INSTALLED_LIST=($(echo $INSTALLED | tr ", " " "))
-for (( i=0; i<${#INSTALLED_LIST[@]}; i++ ))
-do
-	if [[  $i < 5 ]]
+i=0
+for folder in $TARGET_FOLDER/*; do
+	i=$((i+1))
+	if (( $i < $MAX_NUMBER_OF_VERSIONS_INSTALLED ));
 	then
+		echo "Keeping $folder"
 		continue
 	fi
-	ITEM=${INSTALLED_LIST[$i]}
-	echo "Removing $ITEM.."
-	rm -rf "$TARGET_FOLDER/$ITEM"
+	echo "Removing $folder.."
+#	rm -rf "$file"	
 done
 
 echo "Done"
